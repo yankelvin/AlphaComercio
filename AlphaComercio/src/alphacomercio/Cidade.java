2db6c3649d3cc6b47a16b2aba185b724976eea92
Kelvin Murilo <kelvinmuriloas@gmail.com>
2019-05-23 17:27:02 -0300

    Criação das  classes sugeridas no projeto

---
+++ AlphaComercio/src/alphacomercio/Cidade.java
@@ -0,0 +1,9 @@
+package alphacomercio;
+
+import java.util.Map;
+
+public class Cidade {
+	private String nomeCidade;
+	private UnidadeFederecao unidadeFederacao;
+	private Map<String, Bairro> bairros;
+}
---
+++ AlphaComercio/src/alphacomercio/UnidadeFederecao.java
@@ -0,0 +1,8 @@
+package alphacomercio;
+
+import java.util.Map;
+
+public class UnidadeFederecao {
+	private String siglaUnidadeFederacao;
+	private Map<String, Cidade> cidades;
+}
