55236b4eb230bfc921642402859971e6164c7599
Yan Kelvin <yan.kelvin@hotmail.com>
2019-05-25 12:23:10 -0300

    Método de busca por CEP

--- AlphaComercio/src/alphacomercio/AlphaComercio.java
+++ AlphaComercio/src/alphacomercio/AlphaComercio.java
@@ -5,7 +5,7 @@ public class AlphaComercio {
     public static void main(String[] args) {
         Sistema sis = new Sistema();
         sis.lerArquivo();
-
+        sis.buscarLogradouro("99980974");
     }
 
 }
--- AlphaComercio/src/alphacomercio/Sistema.java
+++ AlphaComercio/src/alphacomercio/Sistema.java
@@ -3,18 +3,26 @@ package alphacomercio;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Sistema {
 
     private final HashMap<String, Logradouro> logradouros;
+    private final HashMap<String, ArrayList<Cidade>> cidades;
+    private final HashMap<String, ArrayList<Bairro>> bairros;
 
     public Sistema() {
         this.logradouros = new HashMap<>();
+        this.cidades = new HashMap<>();
+        this.bairros = new HashMap<>();
     }
 
     public void iniciar() {
+        System.out.println("------ MENU ------");
+        System.out.println("1 - Ler arquivo");
+        System.out.println("2 - Fazer busca por CEP\n");
+        System.out.println("");
     }
 
     public void lerArquivo() {
@@ -40,6 +48,7 @@ public class Sistema {
                 Cidade cidade = new Cidade(nomeCidade);
                 String siglaUF = aux[1].substring(aux[1].length() - 2);
                 UnidadeFederacao UF = new UnidadeFederacao(siglaUF);
+                UF.addCidade(cidade);
 
                 logradouro = new Logradouro();
                 logradouro.setCep(cep);
@@ -50,19 +59,20 @@ public class Sistema {
                 if (aux.length >= 3) {
                     String nomeBairro = aux[2];
                     Bairro bairro = new Bairro(nomeBairro);
+                    logradouro.setBairro(bairro);
 
+                    cidade.addBairro(bairro);
                     // Logradouro -> Todas as informações.
                     if (aux.length >= 4) {
                         String nomeLogradouro = aux[3];
-                        logradouro = new Logradouro(cep, cidade, UF, bairro, nomeLogradouro);
+                        logradouro.setNomeLogradouro(nomeLogradouro);
                     }
+                    bairro.addLogradouro(logradouro);
                 }
 
                 this.logradouros.put(cep, logradouro);
                 linha = lerArq.readLine();
             }
-
-            System.out.println(this.logradouros.get("01001000"));
             arq.close();
         } catch (IOException e) {
             System.err.printf("Erro na abertura do arquivo: %s.\n",
@@ -72,7 +82,12 @@ public class Sistema {
         System.out.println();
     }
 
-    public void organizarInformacoes() {
+    public void buscarLogradouro(String cep) {
+        Logradouro logradouro = this.logradouros.get(cep);
+        System.out.println("Nome cidade: " + logradouro.getCidade().getNomeCidade());
+        System.out.println("Nome bairro: " + logradouro.getBairro().getNomeBairro());
+        System.out.println("UF: " + logradouro.getUnidadeFederacao().getSiglaUnidadeFederacao());
+        System.out.println("Nome Logradouro: " + logradouro.getNomeLogradouro());
     }
 
 }
